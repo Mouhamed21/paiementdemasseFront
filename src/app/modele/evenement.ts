c945650901fa476a7fb85f08ac25c7c4a974a8ec
Mouhamed <diallo.mouhamed288@gmail.com>
2022-06-16 10:06:03 +0000

    commit 6 correction des bug sur git

---
+++ src/app/modele/evenement.ts
@@ -0,0 +1,10 @@
+export class Evenement {
+    constructor(
+        public id?: number,
+        public anneeCampagne?: number,
+        public dateCloture?:Date,
+        public dateCreation?:Date,
+        public moisCampagne?: string,
+        public status?: string
+    ) { }
+}
---
+++ src/app/service/evenementService/evenement.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Evenement} from "../../modele/evenement";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EvenementService {
+
+    baseUrl = environment.urlApi ;
+
+    constructor(private httpClient: HttpClient) { }
+    httpOptions =
+        {
+            headers: new HttpHeaders({
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/JSON',
+                'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT'
+            })
+        };
+
+
+    getEvenements() {
+        return this.httpClient.get(this.baseUrl + '/allevenements');
+    }
+
+    postEvenement(evenement:Evenement) {
+        return this.httpClient.post(this.baseUrl + '/new/evenements',  evenement )
+    }
+
+    updateEvenement(id:number,evenement:Evenement) {
+        return this.httpClient.put(this.baseUrl + '/evenement/' + id,  evenement )
+    }
+}
