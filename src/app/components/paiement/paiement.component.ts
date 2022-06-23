caf97e84f9e9797242046477fc2a5319ca2985c6
nazimgueye <nazimgueye17@gmail.com>
2022-06-23 15:37:55 +0000

    paiement1

--- src/app/components/paiement/paiement.component.ts
+++ src/app/components/paiement/paiement.component.ts
@@ -8,6 +8,7 @@ import { PaiementService } from 'src/app/service/paiement.service';
 import { KeycloakService } from 'keycloak-angular';
 import { UserServiceService } from 'src/app/service/userService/user-service.service';
 import { Paiement } from 'src/app/modele/paiement';
+import { StatutService } from 'src/app/service/statut.service';
 
 
 @Component({
@@ -16,6 +17,7 @@ import { Paiement } from 'src/app/modele/paiement';
     styleUrls: ['./paiement.component.scss']
 })
 export class PaiementComponent implements OnInit {
+
     numPensionArechercher
     beneficiaireByNumPension:Beneficiaire = new Beneficiaire()
     displayDialog: boolean
@@ -29,12 +31,24 @@ export class PaiementComponent implements OnInit {
     email
     existenceCaisse:boolean = true
 
+    //concernant les statuts
+    listeDesStatuts
+    statutSelected
+
+    //concenant les pieces
+    typePieceSelected
+    typesDePieces = ["CNI","PASSPORT"]
+
+
+
+
 
     activeIndex1: number = 0;
     activeIndex2: number = 0;
 
     constructor(private beneficiaireService:BeneficiaireService,private paiementService:PaiementService,
-                private keycloakService:KeycloakService,private  userService:UserServiceService) {
+                private keycloakService:KeycloakService,private  userService:UserServiceService,
+                private statutService:StatutService) {
         this.keycloakService.loadUserProfile().then( res =>
         {
             console.log(res);
@@ -46,6 +60,7 @@ export class PaiementComponent implements OnInit {
         });    }
 
     ngOnInit(): void {
+        this.getAllStatus()
         // this.getBeneficiaireByNumPension(23658974)
         console.log(23658974)
     }
@@ -65,9 +80,9 @@ export class PaiementComponent implements OnInit {
     }
 
     updateBeneficiaire(){
-
+        // this.beneficiaireByNumPension.typePiece = this.typePieceSelected
         console.log(this.beneficiaireByNumPension)
-        // this.beneficiaireService.updateBeneficiaire(this.beneficiaireByNumPension.id).subscribe(res=>{
+        // this.beneficiaireService.updateBeneficiaire(this.beneficiaireByNumPension.id,this.beneficiaireByNumPension).subscribe(res=>{
         //     console.log(res)
         // })
     }
@@ -87,20 +102,33 @@ export class PaiementComponent implements OnInit {
         this.paiement.idBureau = this.user.dg_structure.id
         this.paiement.idCaisse = this.user.dg_caisse.id
         this.paiement.idUser = this.user.id
+        this.detailBeneficiaireChoisi.paye = true
+        // this.detailBeneficiaireChoisi.statut = this.statutSelected
+        // this.detailBeneficiaireChoisi.statut = this.listeDesStatuts.filter(s=>s.libelle =0)
+
+
         console.log("OKKKK")
 
         console.log(this.paiement)
-            this.paiementService.savePaiement(this.paiement).subscribe(res=>{
-            console.log(res)
-        })
+        //     this.paiementService.savePaiement(this.paiement).subscribe(res=>{
+        //     console.log(res)
+        // })
     }
 
-    public getUserByEmail(email){
-        console.log(email);
+     getUserByEmail(email){
         return this.userService.getUserByEmail(email).subscribe(data =>
         {
-            console.log(data);
             this.user = data;
         })
     }
+
+    getAllStatus(){
+        this.statutService.getStatuts().subscribe(res=>{
+            this.listeDesStatuts = res
+            console.log(this.listeDesStatuts)
+        })
+    }
+
+
+
 }
